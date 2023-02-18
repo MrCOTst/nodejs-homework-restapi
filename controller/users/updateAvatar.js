8171d13444e74b497f602d5231dc2c4b56a64f1d
MrCOTst <mypostgg@gmail.com>
2023-02-19 00:07:44 +0200

    19.02.2023

--- controller/users/updateAvatar.js
+++ controller/users/updateAvatar.js
@@ -8,17 +8,25 @@ const avatarDir = path.join(__dirname, "../../", "public", "avatars");
 const updateAvatar = async (req, res) => {
   const { path: tempUpload, originalname } = req.file;
   const { _id: id } = req.user;
-  const [extention] = originalname.split(".").reverse();
+  const [, extention] = originalname.split(".");
   const imageName = `${id}.${extention}`;
   const resultUpload = path.join(avatarDir, imageName);
 
   try {
     const image = await Jimp.read(tempUpload);
-    await image.resize(250, 250).writeAsync(tempUpload);
+    await image
+      .autocrop()
+      .cover(
+        250,
+        250,
+        Jimp.HORIZONTAL_ALIGN_CENTER || Jimp.VERTICAL_ALIGN_MIDDLE
+      )
+      .writeAsync(tempUpload);
 
     await fs.rename(tempUpload, resultUpload);
 
-    const avatarUrl = path.join(avatarDir, imageName);
+    const avatarUrl = `/avatars/${imageName}`;
+
     await User.findByIdAndUpdate(id, { avatarUrl });
     res.status(200).json({ avatarUrl });
   } catch (error) {
